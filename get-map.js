0661fee430b0174489323307f84c42b2f8e161b6
Wes White <wtealwhite@gmail.com>
2019-03-19 20:14:53 -0500

    Create get-map.js

---
+++ get-map.js
@@ -0,0 +1,12 @@
+//curry in map
+var objects = [{ id: 1 }, { id: 2 }, { id: 3 }]
+
+//no curry, no reuse
+objects.map(function(o){ return o.id })
+
+//curry
+var curry = require('curry')
+var get = curry(function(property, object){ return object[property] })
+objects.map(get('id')) //= [1, 2, 3]
+
+//https://hughfdjackson.com/javascript/why-curry-helps/
